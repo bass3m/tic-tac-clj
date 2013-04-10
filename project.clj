a52fd92133a2080532a357e6a3c7590218b595ad
Bassem Youssef <bassem@outlook.com>
2013-04-10 11:17:16 -0700

    add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject tic "0.1.0-SNAPSHOT"
+  :description "Tic-Tac_Toe fun"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main tic.server
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [server-socket "1.0.0"]])
